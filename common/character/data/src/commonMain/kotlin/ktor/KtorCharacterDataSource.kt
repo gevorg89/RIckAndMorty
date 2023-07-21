f9a127bc7f9dc2551cfcd3ba89b4d0625164668d
Gevorg Arutyunyan <Gevorg.Arutyunyan@kaspersky.com>
2023-07-21 18:06:35 +0300

    init

--- common/character/data/src/commonMain/kotlin/ktor/KtorCharacterDataSource.kt
+++ common/character/data/src/commonMain/kotlin/ktor/KtorCharacterDataSource.kt
@@ -4,17 +4,12 @@ import Character
 import CharacterRemote
 import io.ktor.client.HttpClient
 import io.ktor.client.request.get
-import io.ktor.client.request.url
 import mappers.toDomain
 
 class KtorCharacterDataSource (private val httpClient: HttpClient) {
     suspend fun fetch() : List<Character> {
         return runCatching {
-            val character = httpClient.get<CharacterRemote> {
-                url {
-                    encodedPath = "api/character"
-                }
-            }
+            val character = httpClient.get<CharacterRemote>("api/character")
             character.results.map { it.toDomain() }
         }.onFailure {
             emptyList<Character>()
