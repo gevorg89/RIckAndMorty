84bc27d64a891e8af08b41a19749d0f1de1c95f6
Gevorg Arutyunyan <Gevorg.Arutyunyan@kaspersky.com>
2023-07-23 18:03:28 +0300

    init

--- common/character/data/src/commonMain/kotlin/sqldelight/KtorSqlDelightDataSource.kt
+++ common/character/data/src/commonMain/kotlin/sqldelight/KtorSqlDelightDataSource.kt
@@ -1,6 +1,25 @@
 package sqldelight
 
-import io.ktor.client.HttpClient
+import core.database.AppDatabase
+import database.Character
 
-class KtorSqlDelightDataSource () {
+class KtorSqlDelightDataSource(database: AppDatabase) {
+
+    private val dbQuery = database.characterQueries
+
+    internal fun removeAllCharacters() {
+        dbQuery.transaction {
+            dbQuery.removeAllCharacters()
+        }
+    }
+
+    internal fun insertCharacter(name : String, image: String) {
+        dbQuery.transaction {
+            dbQuery.insertCharacter(name, image)
+        }
+    }
+
+    internal fun selectAllCharacters(): List<Character> {
+        return dbQuery.selectAllCharacters().executeAsList()
+    }
 }
---
+++ common/core/src/commonMain/kotlin/db/entity/Character.kt
@@ -0,0 +1,3 @@
+package db.entity
+
+data class Character(val name: String, val image: String)
