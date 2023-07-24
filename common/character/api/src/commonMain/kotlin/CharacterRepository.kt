68282c225bab02551ed21be202a770d2b3af0135
Gevorg Arutyunyan <Gevorg.Arutyunyan@kaspersky.com>
2023-07-24 14:14:58 +0300

    init

--- common/character/api/src/commonMain/kotlin/CharacterRepository.kt
+++ common/character/api/src/commonMain/kotlin/CharacterRepository.kt
@@ -1,7 +1,11 @@
 import kotlinx.coroutines.flow.Flow
 
 interface CharacterRepository {
-    suspend fun fetch()
+    suspend fun fetch(page: Int)
     suspend fun invalidate()
+    suspend fun fetchFirstRemote() : Character
+    suspend fun selectFirst() : Character?
+    suspend fun selectCount() : Long
+    suspend fun getCharacters(limit: Long, offset: Long)
     val characters : Flow<List<Character>>
 }
