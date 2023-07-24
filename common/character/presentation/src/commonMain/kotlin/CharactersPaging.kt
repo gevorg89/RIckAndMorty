392cf6386de6749051fa94eff6dfdd36e9850f63
Gevorg Arutyunyan <Gevorg.Arutyunyan@kaspersky.com>
2023-07-24 15:35:34 +0300

    init

--- common/character/presentation/src/commonMain/kotlin/CharactersPaging.kt
+++ common/character/presentation/src/commonMain/kotlin/CharactersPaging.kt
@@ -7,11 +7,14 @@ class CharactersPaging(private val characterRepository: CharacterRepository) : P
     }
 
     override suspend fun checkForFetch(): Boolean {
-        return runCatching {
-            val firstRemote = characterRepository.fetchFirstRemote()
-            val firstLocal = characterRepository.selectFirst()
-            firstLocal == null || firstLocal.name != firstRemote.name
-        }.getOrDefault(false)
+        val firstRemote = characterRepository.fetchFirstRemote()
+        val firstLocal = characterRepository.selectFirst()
+        if (firstLocal == null) {
+            return true
+        } else if (firstLocal.name != firstRemote.name) {
+            return true
+        }
+        return false
     }
 
     override suspend fun getLocalCount(): Long {
