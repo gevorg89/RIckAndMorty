8fa59bdd54b8c8837c6fe67280280d56d7677b16
Gevorg Arutyunyan <Gevorg.Arutyunyan@kaspersky.com>
2023-07-25 16:10:02 +0300

    init

--- common/umbrella/build.gradle.kts
+++ common/umbrella/build.gradle.kts
@@ -29,9 +29,4 @@ kotlin {
 android {
     namespace = "com.gevorg89.rickandmorty.umbrella"
     compileSdk = 33
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
 }
